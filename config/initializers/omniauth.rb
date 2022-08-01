e66ded26478b7209da79c4ac9be8794f21f79c41
Elliot Ng <elliotng@oursky.com>
2022-08-01 14:50:23 +0800

    Add NftAuth OIDC strategy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 require "strategies/nftauth"
+require "strategies/nftauth_oidc"
 Rails.application.config.middleware.use OmniAuth::Builder do
   configure do |config|
     config.path_prefix = '/auth'
@@ -8,4 +9,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :nftauth, client_id: ENV['client_id'],
     client_secret: ENV['client_secret'],
     scope: 'read'
+  provider :nftauth_oidc,
+    client_options: {
+      identifier: ENV['client_id'],
+      secret: ENV['client_secret'],
+      redirect_uri: "http://localhost:3001/auth/nftauth_oidc/callback",
+    }
 end
